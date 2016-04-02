337e000dced0faeb1ddbc24b1bc4f7cb020d7b47
cajoue <cajoue@users.noreply.github.com>
2016-04-02 15:35:28 +0200

    feat: Uses wikipedia API on City name

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
 function loadData() {
 
+    /*
+    The $ that shows up in variable names, like $body for example, is just a character like any other. In this case, it refers to the fact that the variable referenced by $body is a jQuery collection, not a DOM node.
+    */
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -30,22 +33,23 @@ function loadData() {
     // load nytimes
 
     // filter results on type_of_material, newest first, this year, first page (0-9 articles )
-    var myNYTAPI = 'xxxx'
+    // Please replace all the "x"s with your own NYT API key
+    var myNYTAPI = 'xxxxxxxx'
     var nytURL= 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr +
         '&fq=type_of_material:("News")&begin_date=20160101&sort=newest&page0&api-key=' + myNYTAPI;
 
     $.getJSON(nytURL, function(data){
-        //console.log(data);
-        //console.log(data.response.docs);
+        // console.log(data);
+        // console.log(data.response.docs);
         $nytHeaderElem.text('New York Times articles about ' + cityStr);
         var returnedArticles = data.response.docs;
-        console.log(returnedArticles);
+        // console.log(returnedArticles);
         var items = [];
         // $.each(returnedArticles, function(key, val){
         //     items.push("<li id='" + key + "'>" + val + "</li>");
         // });
 
-        for (var i = 0; i < returnedArticles.length; i++) {
+        for (var i = 0, articlesLen = returnedArticles.length; i < articlesLen; i++) {
             items.push("<li class='article'><a href=" + returnedArticles[i].web_url + "'>" + returnedArticles[i].headline.main + "</a>" +
                 "<p>" + returnedArticles[i].snippet + "</p></li>");
             $nytElem.append(items[i]);
@@ -59,6 +63,34 @@ function loadData() {
     }).fail(function(e){
         $nytHeaderElem.text('New York Times Articles Could Not be Loaded');
     });
+
+    // load wikipedia data
+
+    var wpURL = 'https://en.wikipedia.org/w/api.php';
+    var wpBaseURL = 'https://en.wikipedia.org/wiki/';
+    $.ajax(wpURL, {
+        data: {
+            action: 'query',
+            list: 'search',
+            srsearch: cityStr,
+            format: 'json'
+        },
+        dataType: 'jsonp',
+        success: function(data){
+            // do something with data
+            //console.log('wiki title', data.query.search[0].title);
+            var wikiArticles = data.query.search;
+            //console.log('wiki title', wikiArticles[0].title);
+            var items = [];
+            for (var i = 0, wikiLen = wikiArticles.length; i < wikiLen; i++) {
+                //console.log('wiki link: ' + wpBaseURL + wikiArticles[i].title);
+                items.push('<li><a href ="' + wpBaseURL + wikiArticles[i].title + '">' + wikiArticles[i].title +'</a></li>');
+                $wikiElem.append(items[i]);
+            };
+        }
+    });
+
+
 
     return false;
     // explanations for return false:
